fb4d4364cafd808c449910d0ef7ac2da9f5894c8
Linus <linus@friikod.se>
2025-07-21 13:59:22 +0200

    Refactor of settings merge

--- axr_options.py
+++ axr_options.py
@@ -12,7 +12,6 @@ def main():
         axr_orig = file_axr_orig.readlines()
 
     make_original_file_backup(axr_orig)
-
     print_settings_and_original_file_diff(axr_orig, axr_settings)
 
     merged_settings = merge_settings(axr_orig, axr_settings)
@@ -56,49 +55,55 @@ def merge_settings(original: list[str], settings: dict[str, typing.Any]) -> list
     result = original.copy()
     settings_applied: set[str] = set()
     EIGHT_SPACES = " " * 8
+    mcm_settings_index = result.index("[mcm]\n")
+
+    print(mcm_settings_index)
     
-    # First pass: update existing lines that contain our keys
+    # First pass: update existing lines with new settings
     for i, line in enumerate(result):
         original_setting_name = line.strip().split('=')[0].strip() if '=' in line else ''
-        for setting_name, setting_value in settings.items():
-            if setting_name == original_setting_name:
-                result[i] = f"{EIGHT_SPACES}{setting_name} = {setting_value if type(setting_value) is not bool else str(setting_value).lower()}\n"
-                settings_applied.add(setting_name)
-                break
+
+        if original_setting_name in settings and original_setting_name != '':
+            setting_value = settings[original_setting_name]
+            result[i] = f"{EIGHT_SPACES}{original_setting_name} = {setting_value if not isinstance(setting_value, bool) else str(setting_value).lower()}\n"
+            settings_applied.add(original_setting_name)
     
     # Second pass: add any settings that weren't found in the original file
-    mcm_settings_index = result.index("[mcm]\n")
     for (setting_name, setting_value) in settings.items():
         if setting_name in settings_applied:
             continue
 
-        result.insert(mcm_settings_index + 1, f"{EIGHT_SPACES}{setting_name} = {setting_value if type(setting_value) is not bool else str(setting_value).lower()}\n")
+        result.insert(mcm_settings_index + 1, f"{EIGHT_SPACES}{setting_name} = {setting_value if not isinstance(setting_value, bool) else str(setting_value).lower()}\n")
 
     return sort_mcm_settings(result)
 
-def sort_mcm_settings(settings: list[str]) -> list[str]:
-    """ Sort the MCM settings in alphabetical order."""
+def sort_mcm_settings(axr_file_contents: list[str]) -> list[str]:
+    """ Sort the MCM settings in axr file alphabetical order. """
 
     try:
-        mcm_settings, mcm_settings_begin_index, mcm_settings_end_index = get_mcm_settings(settings)
-        return settings[:mcm_settings_begin_index + 1] + mcm_settings + settings[mcm_settings_end_index:]
+        mcm_settings, mcm_settings_begin_index, mcm_settings_end_index = get_mcm_settings(axr_file_contents)
+        return axr_file_contents[:mcm_settings_begin_index + 1] + sorted(mcm_settings) + axr_file_contents[mcm_settings_end_index:]
     except ValueError:
         print("No [mcm] section found in the settings file. Cannot sort MCM settings.")
-        return settings
+        return axr_file_contents
 
-def get_mcm_settings(settings: list[str]) -> tuple[list[str], int, int]:
-    mcm_settings_begin_index = settings.index("[mcm]\n")
+def get_mcm_settings(axr_file_contents: list[str]) -> tuple[list[str], int, int]:
+    """ 
+    Returns the [MCM] section from an axr file.
+    Raises ValueError if [MCM] section is not present.
+    """
+    mcm_settings_begin_index = axr_file_contents.index("[mcm]\n")
     mcm_settings_end_index = None
-    for i in range(mcm_settings_begin_index + 1, len(settings)):
-        if settings[i].startswith("[") or settings[i] == "\n":
+    for i in range(mcm_settings_begin_index + 1, len(axr_file_contents)):
+        if axr_file_contents[i].startswith("[") or axr_file_contents[i] == "\n":
             mcm_settings_end_index = i - 1
             break
 
     # If we have start of mcm but no end, we assume the MCM settings are at the end of the file
     if mcm_settings_end_index is None:
-        mcm_settings_end_index = len(settings)
+        mcm_settings_end_index = len(axr_file_contents)
 
-    mcm_settings = sorted(settings[mcm_settings_begin_index + 1:mcm_settings_end_index])
+    mcm_settings = axr_file_contents[mcm_settings_begin_index + 1:mcm_settings_end_index]
 
     return mcm_settings, mcm_settings_begin_index, mcm_settings_end_index
 
