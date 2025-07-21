d616a3786ac9ae68a649cc67ac8c728e4885c62f
Linus <linus@friikod.se>
2025-07-21 15:38:57 +0200

    Added error handling. Formatted python code with ruff.

--- axr_options.py
+++ axr_options.py
@@ -1,47 +1,64 @@
 import json
 import typing
 
+
 def main():
-    axr_settings: dict[str, typing.Any] = {}
-    axr_orig: list[str] = []
+    try:
+        with open("settings.json", "r") as settings_file:
+            settings = json.load(settings_file)
 
-    with open("settings.json", "r") as settings_file:
-        axr_settings = json.load(settings_file)
+        with open("axr_options.ltx", "r") as file_original:
+            original = file_original.readlines()
 
-    with open("axr_options.ltx", "r") as file_axr_orig:
-        axr_orig = file_axr_orig.readlines()
+        make_file_backup(original, "axr_options_backup.ltx")
+        print_settings_and_original_file_diff(original, settings)
 
-    make_original_file_backup(axr_orig)
-    print_settings_and_original_file_diff(axr_orig, axr_settings)
+        merged_settings = merge_settings(original, settings)
+        with open("axr_options.ltx", "w") as file_original:
+            file_original.writelines(merged_settings)
+    except OSError as error:
+        print("Something went wrong while reading or writing to files.", error)
 
-    merged_settings = merge_settings(axr_orig, axr_settings)
-    with open("axr_options.ltx", "w") as file_axr_orig:
-        file_axr_orig.writelines(merged_settings)
 
-def print_settings_and_original_file_diff(original: list[str], settings: dict[str, str]):
+def print_settings_and_original_file_diff(
+    original: list[str], settings: dict[str, str]
+):
     """
     Check to see if any of our settings are not valid, as in not present in the original file.
     Though they still might be valid but hasn't been created in the original file yet.
     This is mainly a warning to users that they might have settings that are not recognized by the game.
     """
+    try:
+        raw_original_mcm_settings, _, _ = get_mcm_settings(original)
+        original_mcm_settings = {
+            line.strip().split("=")[0].strip() if "=" in line else ""
+            for line in raw_original_mcm_settings
+        }
 
-    raw_original_mcm_settings, _, _ = get_mcm_settings(original)
-    original_mcm_settings = {line.strip().split('=')[0].strip() if '=' in line else '' for line in raw_original_mcm_settings}
-    
-    diff: list[str] = []
-    for new_setting_name, value in settings.items():
-        if new_setting_name not in original_mcm_settings:
-            diff.append(f"{new_setting_name} = {value}")
-    
-    if diff:
-        print("The following settings are not present in the original file:")
-        for line in diff:
-            print(f"  {line}")
-        print("This CAN mean that the game does not recognize these settings. If all settings are working, you can ignore this message.")
+        diff: list[str] = []
+        for new_setting_name, value in settings.items():
+            if new_setting_name not in original_mcm_settings:
+                diff.append(f"{new_setting_name} = {value}")
 
-def make_original_file_backup(original: list[str]) -> None:
-    with open("axr_options_backup.ltx", "w") as file_axr_orig_backup:
-        file_axr_orig_backup.writelines(original)
+        if diff:
+            print("The following settings are not present in the original file:")
+            for line in diff:
+                print(f"  {line}")
+            print(
+                "This CAN mean that the game does not recognize these settings. If all settings are working, you can ignore this message."
+            )
+    except ValueError as error:
+        print(
+            "Could not calculate settings diff. Failed to get mcm settings from original file.",
+            error,
+        )
+
+
+def make_file_backup(file_contents: list[str], path: str) -> None:
+    """Makes a backup with specified file contents at path. Raises OSError on failure."""
+    with open(path, "w") as file:
+        file.writelines(file_contents)
+
 
 def merge_settings(original: list[str], settings: dict[str, typing.Any]) -> list[str]:
     """
@@ -50,30 +67,58 @@ def merge_settings(original: list[str], settings: dict[str, typing.Any]) -> list
     """
     result = original.copy()
     applied_settings: set[str] = set()
-    EIGHT_SPACES = " " * 8 # This is how MCM settings are indented in the original axr_options.ltx
+    EIGHT_SPACES = (
+        " " * 8
+    )  # This is how MCM settings are indented in the original axr_options.ltx
 
-    mcm_settings, mcm_settings_start_index, mcm_settings_end_index = get_mcm_settings(result)
-    
-    # First pass: update existing lines with new settings
-    for i, line in enumerate(mcm_settings):
-        original_setting_name = line.strip().split('=')[0].strip() if '=' in line else ''
+    try:
+        mcm_settings, mcm_settings_start_index, mcm_settings_end_index = (
+            get_mcm_settings(result)
+        )
 
-        if original_setting_name in settings and original_setting_name != '':
-            setting_value = settings[original_setting_name]
-            mcm_settings[i] = f"{EIGHT_SPACES}{original_setting_name} = {setting_value if not isinstance(setting_value, bool) else str(setting_value).lower()}\n"
-            applied_settings.add(original_setting_name)
-    
-    # Second pass: add any settings that weren't found in the original axr_options file
-    unapplied_settings = {k: v for k, v in settings.items() if k not in applied_settings}
-    for setting_name, setting_value in unapplied_settings.items():
-        mcm_settings.insert(len(mcm_settings) - 1, f"{EIGHT_SPACES}{setting_name} = {setting_value if not isinstance(setting_value, bool) else str(setting_value).lower()}\n")
+        # First pass: update existing lines with new settings
+        for i, line in enumerate(mcm_settings):
+            original_setting_name = (
+                line.strip().split("=")[0].strip() if "=" in line else ""
+            )
 
-    result = result[:mcm_settings_start_index + 1] + sorted(mcm_settings) + result[mcm_settings_end_index:]
+            if original_setting_name in settings and original_setting_name != "":
+                setting_value = settings[original_setting_name]
+                mcm_settings[i] = (
+                    f"{EIGHT_SPACES}{original_setting_name} = {setting_value if not isinstance(setting_value, bool) else str(setting_value).lower()}\n"
+                )
+                applied_settings.add(original_setting_name)
 
-    return result
+        # Second pass: add any settings that weren't found in the original axr_options file
+        unapplied_settings = {
+            k: v for k, v in settings.items() if k not in applied_settings
+        }
+        for setting_name, setting_value in unapplied_settings.items():
+            mcm_settings.insert(
+                len(mcm_settings) - 1,
+                f"{EIGHT_SPACES}{setting_name} = {setting_value if not isinstance(setting_value, bool) else str(setting_value).lower()}\n",
+            )
+
+        result = (
+            result[: mcm_settings_start_index + 1]
+            + sorted(mcm_settings)
+            + result[mcm_settings_end_index:]
+        )
+
+        return result
+    except ValueError as error:
+        print(
+            """
+        Could not merge settings. Failed to get mcm settings from original file.
+        This probably means that there is no MCM section in the original file.
+        """,
+            error,
+        )
+        return original
+
 
 def get_mcm_settings(axr_file_contents: list[str]) -> tuple[list[str], int, int]:
-    """ 
+    """
     Returns the [MCM] section from an axr file.
     Raises ValueError if [MCM] section is not present.
     """
@@ -81,16 +126,21 @@ def get_mcm_settings(axr_file_contents: list[str]) -> tuple[list[str], int, int]
     mcm_settings_end_index = None
     for i in range(mcm_settings_begin_index + 1, len(axr_file_contents)):
         if axr_file_contents[i].startswith("[") or axr_file_contents[i] == "\n":
-            mcm_settings_end_index = i - 1 # We only want actual settings, not new lines or brackets
+            mcm_settings_end_index = (
+                i - 1
+            )  # We only want actual settings, not new lines or brackets
             break
 
     # If we have start of mcm but no end, we assume the MCM settings are at the end of the file
     if mcm_settings_end_index is None:
         mcm_settings_end_index = len(axr_file_contents)
 
-    mcm_settings = axr_file_contents[mcm_settings_begin_index + 1:mcm_settings_end_index]
+    mcm_settings = axr_file_contents[
+        mcm_settings_begin_index + 1 : mcm_settings_end_index
+    ]
 
     return mcm_settings, mcm_settings_begin_index, mcm_settings_end_index
+
 
 if __name__ == "__main__":
     main()
