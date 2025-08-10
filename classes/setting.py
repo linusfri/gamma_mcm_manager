88881fd73f9113d87a5ee829f91dec27d32828db
linus <linus.f@protonmail.ch>
2025-08-10 19:10:23 +0200

    refactored how settings are handled. Added function to generate json settings based on the difference between saved ltx file settings and default settings.

--- classes/setting.py
+++ classes/setting.py
@@ -5,5 +5,18 @@ class Setting:
         self.name = name
         self.value = value
 
+    def format_value(self) -> str:
+        try:
+            if not (self.value_is_primitive(self.value)):
+                raise ValueError(f"Value for {self.name} is not a primitive type. Must be str, int, float, bool, or None.")
+
+            return str(self.value).lower() if isinstance(self.value, bool) else str(self.value)
+        except ValueError as e:
+            print(f"Error formatting value for {self.name}: {e}")
+            return str(self.value)
+
     def __repr__(self):
-        return f"Setting(name={self.name}, value={self.value})"
+        return f"{self.name} = {self.format_value()}\n"
+    
+    def value_is_primitive(self, value: Any) -> bool:
+        return isinstance(value, (str, int, float, bool, type(None)))
